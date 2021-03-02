3cf77b8cd63165fafd7e0cbdc1325c5a546ca6b9
dizollate <66797250+dizollate@users.noreply.github.com>
2021-03-02 22:09:04 +0300

    feat: array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  for(let i =0; i < arr.length; i++){
+  	if (arr[i] === '--discard-next' ){
+				arr.splice(i, 2);
+        
+      }
+    if (arr[i] === '--discard-prev'){
+    	  arr.splice(i-1, 2);
+        i--
+    }
+    if (arr[i] === "--double-prev"){
+     	arr.splice(i, 1, arr[i-1]);
+      
+    }
+    if (arr[i] === "--double-next"){
+      arr.splice(i, 1, arr[i+1]);
+     
+    }
+    
+  }
+  return arr
 };
